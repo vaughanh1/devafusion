d882d637c4574bde85aefbb2012b8de187b3d83e
vaughanh1 <56869404+vaughanh1@users.noreply.github.com>
2026-08-23 11:46:59 +0100

    Add Terraform bootstrap infrastructure (#3)
    Co-authored-by: vaughan Hodgson <vaughan.hodgson@lms.com>

---
+++ infrastructure/bootstrap/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-devafusion-tfstate-uks"
+    storage_account_name = "stdevafusiontfstateuks01"
+    container_name       = "tfstate"
+    key                  = "bootstrap.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+
+  features {}
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Product     = "Devafusion"
+    Environment = "shared"
+    Owner       = "Devafusion"
+    CostCentre  = "Devafusion"
+    Purpose     = "Terraform State"
+  }
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
+
+  https_traffic_only_enabled = true
+  min_tls_version            = "TLS1_2"
+
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = true
+  public_network_access_enabled  = true
+  default_to_oauth_authentication = true
+
+  blob_properties {
+    versioning_enabled = true
+
+    delete_retention_policy {
+      days = 30
+    }
+
+    container_delete_retention_policy {
+      days = 30
+    }
+  }
+
+  tags = {
+    Product     = "Devafusion"
+    Environment = "shared"
+    Owner       = "Devafusion"
+    CostCentre  = "Devafusion"
+    Purpose     = "Terraform State"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = var.state_container_name
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
